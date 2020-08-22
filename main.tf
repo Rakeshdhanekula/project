0558efc4ff29683b3e9c7b368e93f8310c36718a
david1412 <davidkumar1412@gmail.com>
2020-08-22 02:15:31 +0200

    Change ubuntu image 1804 to 1603

--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@ resource "aws_security_group" "sessionmanger_security_group" {
   name = "sessionmanager-group"
 }
 
-# Using ami Ubuntu 18.04 
+# Using ami Ubuntu 16.04 
 
 resource "aws_instance" "hitman" {
-  ami                  = "ami-089cc16f7f08c4457"
+  ami                  = "ami-0b4f11f7962010058"  #"ami-089cc16f7f08c4457"
   instance_type        = "t2.micro"
   iam_instance_profile = aws_iam_instance_profile.hitman.name
   tags = {
